1e217f7a1fff79114aa9ca07ae77ca24c2699e4e
murilomarinho <murilomarinho@4392d9c1-53a7-476a-8290-df5c8d09eb13>
2013-04-04 20:49:45 +0000

    Added WAM4.h to the robot_dh folder.

---
+++ robot_dh/WAM4.h
@@ -0,0 +1,34 @@
+/**
+WAM Robot DH Parameters
+
+*/
+
+#ifndef DQ_ROBOTICS_WAM_DH_H
+#define DQ_ROBOTICS_WAM_DH_H
+
+#include"../DQ_kinematics.h"
+#include<Eigen/Dense>
+#include<cmath>
+
+using namespace Eigen;
+
+namespace DQ_robotics{
+
+    DQ_kinematics Wam4Kinematics()
+    {
+        const double pi2 = M_PI_2;
+
+	    Matrix<double,4,4> wam4_dh(4,4);
+	    wam4_dh << 0,    0,    0,      0,      
+                  0,    0,    0.55,   0,
+                  0,    0,    0.045, -0.045,
+                  -pi2, pi2, -pi2,    pi2;
+
+	    DQ_kinematics wam4(wam4_dh,"standard");
+
+        return wam4;        
+    };
+
+}
+
+#endif
