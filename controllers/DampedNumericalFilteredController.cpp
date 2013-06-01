876f6267679a6f8a3cce88eab349402cfba53e0c
murilomarinho <murilomarinho@4392d9c1-53a7-476a-8290-df5c8d09eb13>
2013-06-01 16:43:43 +0000

    In DampedNumericalFilterdController.cpp * 2013/06/01 - Murilo Marques Marinho (murilomarinho@lara.unb.br)
                 - Fixed the minimun singular value considered in the 
                   numerical filtering. It was considering the last 
                   singular value of the matrix when it should be consi-
                   dering the 6th.
                 - Changed the inversion algorithm in the damped pseudo-
                   inverse calculation. It was using .inverse(), which
                   is only recomended for matrix.size() <= 4. Changed it
                   to use Cholesky decomposition as it is always a posi-
                   tive definite matrix.
                 - Minor changes in the included files organization.
    
    In schunk_allcontrollers_example.cpp
    Added new optional performance measurements and changed
    controller tolerance and DampedNumericalFiltered controller
    epsilon.

--- controllers/DampedNumericalFilteredController.cpp
+++ controllers/DampedNumericalFilteredController.cpp
@@ -1,9 +1,30 @@
+/**
+* Chiaverini's Singularity Robust controller for the unit dual quaternion space.
+*
+* \author Murilo Marques Marinho (murilomarinho@lara.unb.br)
+* \since 07/2012
+***********************************************************
+*              REVISION HISTORY
+***********************************************************
+* YYYY/MM/DD - Author (e-mail address)
+*            - Description
+***********************************************************
+* 2013/06/01 - Murilo Marques Marinho (murilomarinho@lara.unb.br)
+             - Fixed the minimun singular value considered in the 
+               numerical filtering. It was considering the last 
+               singular value of the matrix when it should be consi-
+               dering the 6th.
+             - Changed the inversion algorithm in the damped pseudo-
+               inverse calculation. It was using .inverse(), which
+               is only recomended for matrix.size() <= 4. Changed it
+               to use Cholesky decomposition as it is always a posi-
+               tive definite matrix.
+             - Minor changes in the included files organization.
+***********************************************************
+*/
+
+
 #include "DampedNumericalFilteredController.h"
-#include <Eigen/Dense>
-#include <iostream>
-
-using namespace Eigen;
-
 
 namespace DQ_robotics
 {
@@ -27,7 +48,6 @@ DampedNumericalFilteredController::DampedNumericalFilteredController(DQ_kinemati
     delta_thetas_   = MatrixXd::Zero(robot_dofs_,1);
 
     task_jacobian_     = MatrixXd(8,robot_dofs_);
-    task_jacobian_pseudoinverse_   = MatrixXd(robot_dofs_,8);
     svd_ = JacobiSVD<MatrixXd>(robot_dofs_,8);
     svd_sigma_inverted_ = MatrixXd::Zero(robot_dofs_,8);
     identity_           = Matrix<double,8,8>::Identity();
@@ -71,15 +91,22 @@ VectorXd DampedNumericalFilteredController::getNewJointVelocities(DQ reference, 
     singular_values_ = svd_.singularValues();
 
     //Damping Calculation
-    double sigma_min = singular_values_(robot_dofs_-1);
-    VectorXd u_min = svd_.matrixU().col(robot_dofs_-1);
+    double sigma_min = singular_values_(5);
+    VectorXd u_min = svd_.matrixU().col(5);
     double lambda = lambda_max_;
     if (sigma_min < epsilon_)
     {
         lambda = (1-(sigma_min/epsilon_)*(sigma_min/epsilon_))*lambda_max_*lambda_max_;
     }
 
-    task_jacobian_pseudoinverse_ = (task_jacobian_.transpose())*((task_jacobian_*task_jacobian_.transpose() + (beta_*beta_)*identity_ + (lambda*lambda)*u_min*u_min.transpose()).inverse());
+    //task_jacobian_pseudoinverse_ = (task_jacobian_.transpose())*((task_jacobian_*task_jacobian_.transpose() + (beta_*beta_)*identity_ + (lambda*lambda)*u_min*u_min.transpose()).inverse());
+
+    //We want to solve the equation J+ = J^T(JJ^T+aI)^-1, in which the matrix 
+    //being inverted is obviously positive definite if a > 0.
+    //The solver gives us the solution to X = A^-1.B
+    //Therefore I chose to find X^T = B^T(A^T)^-1 then take the transpose of X.
+    task_jacobian_pseudoinverse_ = ((task_jacobian_*task_jacobian_.transpose() + (beta_*beta_)*identity_ + (lambda*lambda)*u_min*u_min.transpose()).transpose()).ldlt().solve(task_jacobian_);
+    task_jacobian_pseudoinverse_.transposeInPlace();
 
     delta_thetas_ = task_jacobian_pseudoinverse_*kp_*error_;
 
