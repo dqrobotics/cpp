788af9faca1afb795a781d2392cc9ada32f0d714
murilomarinho <murilomarinho@4392d9c1-53a7-476a-8290-df5c8d09eb13>
2013-11-18 18:53:42 +0000

    C++: Updated the Schunk.h DH parameter file.
    This file was using older parameters, making the icra2014 laparoscopy demo not work.

--- trunk/C++/robot_dh/Schunk.h
+++ trunk/C++/robot_dh/Schunk.h
@@ -18,16 +18,15 @@ namespace DQ_robotics{
 
     DQ_kinematics SchunkKinematics()
     {
-        const double pi2 = M_PI_2;
-	    Matrix<double,5,7> schunk_dh(5,7);
+      const double pi2 = M_PI_2;
+	    Matrix<double,4,7> schunk_dh(4,7);
 	    schunk_dh << 0,     0,   0,     0,   0,      0,  0,
-	                 0.3,   0,   0.328, 0,   0.2765, 0,  0.40049,
+	                 0.3,   0,   0.328, 0,   0.2765, 0,  0.1793,
 	                 0,     0,   0,     0,   0,      0,  0,
-	                -pi2,   pi2,-pi2,   pi2,-pi2,    pi2,0,
-                     0,     0,   0,     0,   0,      0,  0;
+	                -pi2,   pi2,-pi2,   pi2,-pi2,    pi2,0;
 	    DQ_kinematics schunk(schunk_dh,"standard");
 
-        return schunk;        
+      return schunk;        
     };
 
 }
