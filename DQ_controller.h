afe4e1163d692a12b7f2f9fa45a1b0385b6e29f7
murilomarinho <murilomarinho@4392d9c1-53a7-476a-8290-df5c8d09eb13>
2013-11-20 18:17:28 +0000

    C++: Updated controllers.
    Added const qualifier and reference argument to Controller.h functions.
    
    DampedNumericalFilteredController.cpp/h
    - Added ki and kp gains to control loop.

--- DQ_controller.h
+++ DQ_controller.h
@@ -1,7 +1,18 @@
 /**
-*  All controllers should inherit this class.
-*/
-
+* All controllers should inherit this class.
+*
+* \author Murilo Marques Marinho (murilomarinho@lara.unb.br)
+* \since 2012/07
+***********************************************************
+*              REVISION HISTORY
+***********************************************************
+* YYYY/MM/DD   Author (e-mail address)
+*            - Description
+***********************************************************
+* 2013/11/22   Murilo Marques Marinho (murilomarinho@lara.unb.br)
+             - Added const qualifiers and references whenever
+               possible.
+**/
 
 #ifndef DQCONTROLLER_H
 #define DQCONTROLLER_H
@@ -28,8 +39,8 @@ public: //methods
     DQ_controller(){};
     ~DQ_controller(){};
 
-    virtual VectorXd getNewJointPositions(DQ reference, VectorXd thetas)=0;
-    virtual VectorXd getNewJointVelocities(DQ reference, VectorXd thetas)=0;
+    virtual VectorXd getNewJointPositions( const DQ& reference, const VectorXd& thetas)=0;
+    virtual VectorXd getNewJointVelocities( const DQ& reference, const VectorXd& thetas)=0;
 
 private: //methods
 
