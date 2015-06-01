a138420088c3a2a889d0d8ba3c46e72d7ad30281
murilomarinho <murilomarinho@4392d9c1-53a7-476a-8290-df5c8d09eb13>
2015-06-01 12:02:07 +0000

    [c++] Corrected AX18 robot DH.

--- robot_dh/AX18.h
+++ robot_dh/AX18.h
@@ -21,11 +21,12 @@ namespace DQ_robotics{
         const double pi2 = M_PI_2;
 
 	    Matrix<double,5,6> ax18_dh(5,6);
-	    ax18_dh <<  -pi2,  0,   -pi2,  0,     -pi2,  0,
-                     0,    159,  0,    22.25,  0,    0,
-                     0,    0,   -pi2,  0,     -pi2,  0,
+	    ax18_dh <<   0,    0,   -pi2,  0,     -pi2,  0,
                      167,  0,    0,    81.5,   41,   0,
+                     0,    159,  0,    22.25,  0,    0,
+                    -pi2,  0,   -pi2,  0,     -pi2,  0,
                      0,    0,    0,    1,      0,    0;
+
 	    DQ_kinematics ax18(ax18_dh,"standard");
 
         return ax18;        
