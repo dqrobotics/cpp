cc7a1bfef306892e35949139a7b5bead0364a12c
murilomarinho <murilomarinho@4392d9c1-53a7-476a-8290-df5c8d09eb13>
2017-08-27 09:55:56 +0000

    C++: Fixed AX18M.h as in ticket #24

--- trunk/C++/robot_dh/AX18.h
+++ trunk/C++/robot_dh/AX18.h
@@ -1,9 +1,18 @@
 /**
-ax18 Robot DH Parameters
+ ax18 Robot DH Parameters
+ 
+ \author Murilo Marques Marinho
+ \since 02/2013
+ */
 
-\author Murilo Marques Marinho
-\since 02/2013
-*/
+/*
+ Modified version
+ author Juan José Quiroz
+ since 06/2016
+ changes:
+ -Virtual Joint was removed
+ -Robot dimensions are in meter now.
+ */
 
 #ifndef DQ_ROBOTICS_AX18_DH_H
 #define DQ_ROBOTICS_AX18_DH_H
@@ -15,23 +24,23 @@ ax18 Robot DH Parameters
 using namespace Eigen;
 
 namespace DQ_robotics{
-
-    DQ_kinematics Ax18Kinematics()
+    
+    DQ_kinematics Ax18mKinematics()
     {
         const double pi2 = M_PI_2;
-
-	    Matrix<double,5,6> ax18_dh(5,6);
-	    ax18_dh <<   0,    0,   -pi2,  0,     -pi2,  0,
-                     167,  0,    0,    81.5,   41,   0,
-                     0,    159,  0,    22.25,  0,    0,
-                    -pi2,  0,   -pi2,  0,     -pi2,  0,
-                     0,    0,    0,    1,      0,    0;
-
-	    DQ_kinematics ax18(ax18_dh,"standard");
-
-        return ax18;        
+        
+        Matrix<double,5,5> ax18m_dh(5,5);
+        ax18m_dh <<   0,    0,   -pi2,  -pi2,   -pi2,
+        0.167,  0,    0,    0.1225,    0,
+        0,    0.159,   0.02225,  0 ,   0,
+        -pi2,  0,   -pi2,      -pi2,    0,
+        0,    0,    0,    0,           0;
+        
+        DQ_kinematics ax18m(ax18m_dh,"standard");
+        
+        return ax18m;
     };
-
+    
 }
 
 #endif
