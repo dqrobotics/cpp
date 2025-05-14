edfa46d57566cafb93eede9c2c185697029584bb
Juan José Quiroz Omaña <juanjqogm@gmail.com>
2025-05-14 18:57:08 +0100

    [DQ_SerialManipulator.{h,cpp}] Added the _check_joint_types(), and {set,get}_joint_{type, types} methods.

--- include/dqrobotics/robot_modeling/DQ_SerialManipulator.h
+++ include/dqrobotics/robot_modeling/DQ_SerialManipulator.h
@@ -1,6 +1,6 @@
 #pragma once
 /**
-(C) Copyright 2011-2022 DQ Robotics Developers
+(C) Copyright 2011-2025 DQ Robotics Developers
 
 This file is part of DQ Robotics.
 
@@ -20,9 +20,14 @@ This file is part of DQ Robotics.
 Contributors:
 1. Murilo M. Marinho        (murilomarinho@ieee.org)
 2. Mateus Rodrigues Martins (martinsrmateus@gmail.com)
+
+3. Juan Jose Quiroz Omana (juanjose.quirozomana@manchester.ac.uk)
+    - Added the joint_types member, and the following methods:
+      _check_joint_types(), and {set,get}_joint_{type, types}.
 */
 
 #include <dqrobotics/robot_modeling/DQ_Kinematics.h>
+#include <dqrobotics/robot_modeling/DQ_JointType.h>
 
 namespace DQ_robotics
 {
@@ -31,8 +36,9 @@ class DQ_SerialManipulator: public DQ_Kinematics
 {
 protected:
     DQ curr_effector_;
-
+    std::vector<DQ_JointType> joint_types_;
     DQ_SerialManipulator(const int& dofs);
+    void _check_joint_types() const;
 public:
     DQ get_effector() const;
     DQ set_effector(const DQ& new_effector);
@@ -46,6 +52,10 @@ public:
     VectorXd get_upper_q_dot_limit() const;
     void     set_upper_q_dot_limit(const VectorXd &upper_q_dot_limit);
 
+    DQ_JointType                get_joint_type(const int& ith_joint) const;
+    std::vector<DQ_JointType>   get_joint_types() const;
+    void  set_joint_type(const DQ_JointType& joint_type, const int& ith_joint);
+    void  set_joint_types(const std::vector<DQ_JointType>& joint_types);
 
     //Virtual
     virtual MatrixXd raw_pose_jacobian(const VectorXd& q_vec) const;
@@ -56,6 +66,7 @@ public:
     virtual MatrixXd raw_pose_jacobian(const VectorXd& q_vec, const int& to_ith_link) const = 0;
     virtual MatrixXd raw_pose_jacobian_derivative(const VectorXd& q, const VectorXd& q_dot, const int& to_ith_link) const = 0;
     virtual DQ raw_fkm(const VectorXd& q_vec, const int& to_ith_link) const = 0;
+    virtual std::vector<DQ_JointType> get_supported_joint_types() const = 0;
 
     //Overrides from DQ_Kinematics
     virtual DQ fkm(const VectorXd& q_vec) const override; //Override from DQ_Kinematics
