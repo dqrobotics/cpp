c9e84a7021320f178a459350658177120ffd1c26
Juan José Quiroz Omaña <juanjqogm@gmail.com>
2025-05-14 17:50:23 +0100

    [DQ_JointType] Added a new class to represent the joint types.

---
+++ include/dqrobotics/robot_modeling/DQ_JointType.h
@@ -0,0 +1,43 @@
+/**
+(C) Copyright 2011-2025 DQ Robotics Developers
+
+This file is part of DQ Robotics.
+
+    DQ Robotics is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    DQ Robotics is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with DQ Robotics.  If not, see <http://www.gnu.org/licenses/>.
+
+Contributors:
+1. Juan Jose Quiroz Omana (juanjose.quirozomana@manchester.ac.uk)
+    - Responsible for the original implementation.
+*/
+
+
+#pragma once
+
+namespace DQ_robotics
+{
+class DQ_JointType
+{
+public:
+    enum TYPE{
+        REVOLUTE    = 0,
+        PRISMATIC,
+        SPHERICAL,
+        CYLINDRICAL,
+        PLANAR,
+        SIX_DOF,
+        HELICAL
+    };
+};
+
+}
