69e96efd073d6df3c6ab109f0d1ba2342f6b80de
Juan José Quiroz Omaña <juanjqogm@gmail.com>
2025-05-15 10:03:10 +0100

    [DQ_SerialManipulatorDenso] Added the get_supported_joint_types() method

--- include/dqrobotics/robot_modeling/DQ_SerialManipulatorDenso.h
+++ include/dqrobotics/robot_modeling/DQ_SerialManipulatorDenso.h
@@ -1,6 +1,6 @@
 #pragma once
 /**
-(C) Copyright 2021-2022 DQ Robotics Developers
+(C) Copyright 2011-2025 DQ Robotics Developers
 
 This file is part of DQ Robotics.
 
@@ -17,8 +17,13 @@ This file is part of DQ Robotics.
     You should have received a copy of the GNU Lesser General Public License
     along with DQ Robotics.  If not, see <http://www.gnu.org/licenses/>.
 
+
 Contributors:
-- Murilo M. Marinho (murilomarinho@ieee.org)
+1. Murilo M. Marinho (murilomarinho@ieee.org)
+    - Responsible for the original implementation.
+
+2. Juan Jose Quiroz Omana (juanjose.quirozomana@manchester.ac.uk)
+    - Added the get_supported_joint_types() method.
 */
 
 #include <dqrobotics/robot_modeling/DQ_SerialManipulator.h>
@@ -33,6 +38,7 @@ protected:
 
     DQ _denso2dh(const double& q, const int& ith) const;
 public:
+    std::vector<DQ_JointType> get_supported_joint_types()const override;
 
     // Deprecated on 22.04, will be removed on the next release.
     [[deprecated("Use ? instead.")]] VectorXd get_as() const;
