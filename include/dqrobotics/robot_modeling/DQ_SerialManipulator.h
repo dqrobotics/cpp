7abe22435bba93ec8d8b317e7fe35913188dfcb3
Murilo M Marinho <46012516+mmmarinho@users.noreply.github.com>
2026-09-21 04:37:10 +0100

    Update include/dqrobotics/robot_modeling/DQ_SerialManipulator.h
    Co-authored-by: Juan José Quiroz Omaña <juanjqogm@gmail.com>

--- include/dqrobotics/robot_modeling/DQ_SerialManipulator.h
+++ include/dqrobotics/robot_modeling/DQ_SerialManipulator.h
@@ -214,6 +214,11 @@ public:
      *
      * This is a pure virtual interface contract and must be implemented by subclasses.
      *
+     * @note This function does not take into account any reference-frame or end-effector
+     * displacements, and is intended mostly for internal use by DQ_Kinematics and its
+     * subclasses. Most users should call pose_jacobian_derivative() instead, which applies
+     * those transformations.
+     *
      * @param q Vector containing the robot joint configurations.
      * @param q_dot Vector containing the robot joint velocities.
      * @param to_ith_link Index of the last link to be accounted for in the computation.
